43a8279c3322c26e98d40a2a30f6d9b7f34ecbb0
Savioli Pri <stobbeglaucya@gmail.com>
2021-08-29 19:11:33 -0300

    Adiciona resoluçao exercicio ola mundo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Olamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo, eu me chamo Glaucya!");
+        }
+    }
+}
